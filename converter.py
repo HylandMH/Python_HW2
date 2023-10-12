b30fbc7ed322cb23e7b00b052dd879748fdf1deb
Max <hyland.max@gmail.com>
2023-10-12 15:14:08 -0600

    new file:   converter.py 	modified:   converter_main.py

---
+++ converter.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# Converter module
+
+# Create converter fuction
+# Converter from F > C
+# Converter from C > F
+# Return either result 
--- converter_main.py
+++ converter_main.py
@@ -2,21 +2,26 @@
 # Take input from the user to convert either to 
 
 # Import converter module
-
+import converter as CONVERT
 
 # Function to display title for the program.
 def display_title():
     print("Fahrenheit & Celcius Converter")
     print()
 
+# Ask user either to convert to F -> C, or C -> F.
 
 def main():
     # Call display function
     display_title()
     # While loop for user to repeatedly go multiple times.
-    # Ask user either to convert to F -> C, or C -> F.
-    # Call converter function.
-    # Ask user if they want to go again.
+    again = "y"
+
+    while again.lower() == "y":
+        again = input("Convert again? (y/n): ")
+        print()
+        # Call converter function.
+        # Ask user if they want to go again.
     #End loop and end program
     pass
 
