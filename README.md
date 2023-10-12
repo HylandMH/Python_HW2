a4f158addabcd82e63d4e96bf769b5599ff1016c
Max <hyland.max@gmail.com>
2023-10-12 14:01:45 -0600

    modified:   README.md 	new file:   converter_main.py

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Python_HW2
+# This program is to accept inputs from Fahrenheit or Celsius to then convert those values into the opposition (Fahrenheit of Celsius)
---
+++ converter_main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Import converter module
+
+
+# Function to display title for the program.
+def display_title():
+    print("Fahrenheit & Celcius Converter")
+    print()
+
+
+def main():
+    # Call display function
+    display_title()
+    # While loop for user to repeatedly go multiple times.
+    # Call converter function.
+    # Ask user if they want to go again.
+    #End loop and end program
+    pass
+
+print("Bye!")
+
+if __name__ == "__main__":
+    main()
+
