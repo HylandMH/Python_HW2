e39d85ab4b92adcafde97148a22a34da5ce299ab
Max <hyland.max@gmail.com>
2023-10-15 20:19:33 -0600

    modified:   converter.py

--- converter.py
+++ converter.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 # Converter module
-
-# Create converter fuction
-# Converter from F > C
+# Converter from F < C
+def temp_to_celsius():
+    c = (f - 32)* 5/9
+    return c
 # Converter from C > F
+def temp_to_farenheit():
+    f = (c * 9/5) + 32
+    return c
 # Return either result 
+
